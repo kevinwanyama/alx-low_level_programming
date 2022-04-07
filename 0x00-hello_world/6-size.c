22fa5cefdcbb4540a9e370e0a7b7af5e08672ac8
kevinwanyama <kvnwanyama@gmail.com>
2022-04-07 11:47:38 -0700

    correct 6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,8 @@
 
 /**
  * main - Entry point
- *
  * Return: Always 0 (Success)
- * */
+ */
 int main(void)
 {
 	printf("Size of a char: %zu byte(s)\n", sizeof(char));
@@ -12,4 +11,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return(0);
 }
