bfd9d3f904239cdaca878bc4eaea64c21ad569b5
kevinwanyama <kvnwanyama@gmail.com>
2022-04-08 06:27:06 -0700

    101 first

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- * possible comb of 3 numbers
- * return zero if true
+ * main -  print possible  three digits
+ * Return: always 0 (succees)
  */
 int main(void)
 {
 	int n, m, l;
 
-	for (n=48; n < 58; n++)
+	for (n = 48; n < 58; n++)
 	{
 		for (m = 49; m < 58; m++)
 		{
 			for (l = 50; l < 58; l++)
 			{
-				if (l>m && m > n)
+				if (l > m && m > n)
 				{
 				       	putchar(n);
 					putchar(m);
