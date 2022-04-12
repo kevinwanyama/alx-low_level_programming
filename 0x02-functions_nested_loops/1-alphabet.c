42fe4bd69391e4446bc5dc760d9171fad2806afa
kevinwanyama <kvnwanyama@gmail.com>
2022-04-12 04:20:06 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "head.h"
+/**
+ * prints_alphabet - print the  alphabet, in lowercase,followeed by a new line
+ */
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+	
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
+
