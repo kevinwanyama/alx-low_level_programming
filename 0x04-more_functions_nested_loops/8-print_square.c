f8bbdd437db3cc04232c699321f2b0137a1d5e8e
kevin wanyama <56598203+kevinwanyama@users.noreply.github.com>
2022-04-14 15:57:11 +0300

    fix error

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,24 +1,28 @@
 #include "main.h"
 /**
- * print_square - prints a squire, followed by a new number
- * @size: size of the squire
+ *print_square - prints square of size size.
+ *@size: size of square.
+ *
+ *Return: void.
  */
 void print_square(int size)
 {
+	int i, j;
+
 	if (size <= 0)
 	{
-		-putchar('\n');
-	} else
+		_putchar('\n');
+	}
+	else
 	{
-		int i, j;
-		
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			_putchar('#');
+			for (j = 1; j < size; j++)
 			{
 				_putchar('#');
 			}
-			_putchar('\n');
+		_putchar('\n');
 		}
 	}
 }
