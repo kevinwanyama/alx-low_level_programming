71e953b18691c140d02e34ac64bb7d553ca43b74
kevin wanyama <56598203+kevinwanyama@users.noreply.github.com>
2022-04-14 15:53:18 +0300

    fixing

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,25 +1,21 @@
 #include "main.h"
 /**
- * print_number - prints an integer
- * @n: integer to be printed
+ *print_number - print integer.
+ *@n: integer.
+ *
+ *Return: void.
  */
 void print_number(int n)
 {
-	unsigned int n1;
+	unsigned int x;
 
+	x = n;
 	if (n < 0)
 	{
-		n1 = -n;
-		_putchar('-');
-	} else
-	{
-		n1 = n;
+		_putchar(45);
+		x = -n;
 	}
-	
-	if (n1 / 10)
-	{
-		print_number(n1 / 10);
-	}
-
-	_putchar((n1 % 10) + '0');
+	if (x / 10)
+		print_number(x / 10);
+	_putchar((x % 10) + '0');
 }
