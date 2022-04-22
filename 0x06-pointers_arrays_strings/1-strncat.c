0c88ed3efd51a015f3a5996029b1200e538d1f5a
kevinwanyama <kvnwanyama@gmail.com>
2022-04-22 02:41:40 -0700

    few more changes on the second commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,24 @@
 #include "main.h"
 /**
- * _strcat - concatenates two strings
+ * _strncat - concatenates two strings
  * @dest: A pointer to an char that will be updated
  * @src: A pointer to an char that will be updated
- * Return: dest
+ * @n: value
+ * *Return: dest
  */
-char *_strcat(char *dest, char *src)
+
+char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
 
 	i = 0;
-	while (dest[i] != '\0') /* WHILE count character */
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
 	j = 0;
-	while (src[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - function that encodes a string into 1337
+ *@str: character to be checked
+ * Return: if seperator return 1. Otherwise return 0;
+ */
+char *leet(char *str)
+{
+	int count = 0, i = 0;
+	char *letters = "aeotlAEOTL";
+	char *num = "4307143071";
+
+	while (str[count] != '\0')
+	{
+		i = 0;
+		while (letters[i] != '\0')
+		{
+			if (str[count] == letters[i])
+				str[count] = nums[i];
+			i++;
+		}
+		count++;
+	}
+
+	return (str);
+}
