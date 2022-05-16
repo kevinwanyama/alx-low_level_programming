429f010b17e75cd2b9133238cbc29fe1f7a390dc
kevin wanyama <56598203+kevinwanyama@users.noreply.github.com>
2022-05-16 17:09:34 +0300

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,14 @@
-try me oury
-:wq
+#include "function_pointers.h"
 
+/**
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
